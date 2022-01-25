1cdeb08dcfcef3844ce1fbd9e58e450545126fcf
zhengchuangjun <349373391@qq.com>
2022-01-25 17:00:17 +0800

    提交git操作

--- ygp.js
+++ ygp.js
@@ -1,2 +1,6 @@
-查看远程分支 git branch -r
-查看本地分支 git branch
+git branch -r // 查看远程分支 
+git branch // 查看本地分支 
+git choutout . // 丢弃本地修改
+git choutout master // 本地切换到master,方便删除本地误创建的分支
+git branch -D develop // 强制删除本地错误分支
+gir checkout -b develop origin/develop // 拉去远程分支到本地并关联
